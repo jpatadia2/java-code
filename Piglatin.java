13e1c119bdb7b718216f829ab353f8bee42a6a8a
Jalpesh Patadia <jpatadia@yahoo.com>
2017-07-16 12:16:46 -0600

    Added piglatin example

---
+++ Piglatin.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Simple piglatin generator of words.
+ * Pass in words. Example "java Piglatin jalpesh patadia always eats omelet"
+ * Created by jalpesh on 7/16/2017.
+ */
+public class Piglatin {
+
+    public static void main(String[] args) {
+        generatePiglatin(args);
+    }
+
+    public static void generatePiglatin(String[] words) {
+        List<String> piglatin = new ArrayList<>();
+        for(String w : words) {
+            String first = Character.toString(w.charAt(0));
+            if("aieou".contains(first))
+                piglatin.add(w+"way");
+            else
+                piglatin.add(w.substring(1) + first + "ay");
+        }
+
+        System.out.println("Original:" + Arrays.stream(words).collect(Collectors.joining(",")));
+        System.out.println("Piglatin:" + piglatin.stream().collect(Collectors.joining(",")));
+    }
+}
